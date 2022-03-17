b167bb1576eb20715a817124c2df77677bc81bed
ivnvalex <ivnvalex@gmail.com>
2022-03-17 08:29:13 +0300

    add parameters sheet

--- params.py
+++ params.py
@@ -7,10 +7,11 @@ labels = ('Cars', 'Plants', 'Stuff')
 base_path = os.getcwd()
 initial_bundle_path = os.path.join(base_path, 'initial_bundle')
 test_bundle_path = os.path.join(base_path, 'test_bundle')
-model_path = os.path.join(base_path, 'model')
+model_path = os.path.join(base_path, 'model.h5')
+model_history_plot_path = os.path.join(base_path, 'model_history.png')
 
 # data split parameters
-val_size = 0.1
+val_size = 0.2
 initial_images_n = 0
 for class_ in os.listdir(initial_bundle_path):
     if class_.startswith('.'):
@@ -19,12 +20,13 @@ for class_ in os.listdir(initial_bundle_path):
 val_images_n = int(initial_images_n * val_size)
 train_images_n = initial_images_n - val_images_n
 
-# image parameters
+# img parameters
 img_width = 224
 img_height = 224
 img_channels = 3
 img_shape = (img_width, img_height, img_channels)
 
 # model hyperparameters
+learning_rate = 1e-4
 batch_size = 32
 epochs = 30
